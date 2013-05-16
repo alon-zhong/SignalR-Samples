8929cd0734258d686db814c3bbfde626b2b85b06
Gustavo Armenta <gustavo_armenta@yahoo.com>
2013-05-16 15:31:53 -0700

    add Startup.cs

---
+++ WebApp/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using Owin;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HubConfiguration hubConfiguration = new HubConfiguration()
+            {
+                EnableCrossDomain = true,
+                EnableDetailedErrors = true,
+                EnableJavaScriptProxies = true,
+            };
+            app.MapHubs(hubConfiguration);
+        }
+    }
+}
